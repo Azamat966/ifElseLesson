642d9b942630080d957a5be9bac1b13c959236bc
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 18:00:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System//        double eur = dauble1 * 95.92;.out.println("Введите валюту");
+//        String st = scanner.nextLine();
+//        System.out.println("Ввудите суммы");
+//        double dauble1 = scanner.nextByte();
+//        double usd = dauble1 * 87.30;
+//        double rub = dauble1 * 1.15;
+//        switch (st){
+//            case "USD":
+//                System.out.println(usd);
+//                break;
+//            case "RUB":
+//                System.out.println(rub);
+//                break;
+//            case "EUR":
+//                System.out.println(eur);
+//                break;
+//            default:
+
+
+        }
+//        String a = scanner.nextLine();
+//        switch (a) {
+//            case "a":
+//                System.out.println(1);
+////                break;
+//            case "b":
+//                System.out.println(2);
+////                break;
+//            default:
+//                System.out.println(3);
+////                break;
+//        }
+
+
+    }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String sr= scanner.nextLine();
+//        if (sr.equals("USD")){
+//            System.out.println("84.5");
+//        } else if (sr.equals("EUR")) {
+//            System.out.println("102.58");
+//        } else if (sr.equals("RUB")) {
+//            System.out.println("1.15");
+//        } else if (sr.equals("KZT")) {
+//            System.out.println("0.20");
+//        }else
+//            System.out.println("Такой валюты нет");
+//
+//    }
+
+//}
+
+
+//    public static void main(String[] args) {
+////        Scanner scanner = new Scanner(System.in);
+////        }else if (b > b && b >c){
+////            System.out.println(a);
+////            System.out.println(b);
+////        } else
+////            System.out.println(c);
+////        int a = scanner.nextInt();
+////        int b = scanner.nextInt();
+////        int c = scanner.nextInt();
+////        if (a > b) {
+
+
+
+
+
+
+
+
